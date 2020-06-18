4353c229075e4b2f7a3c984e3857d21c62b4cff8
Marcel van der Meer <marcelmeer@gmail.com>
2020-06-18 10:33:50 +0200

    PUT-method added

--- api-client.js
+++ api-client.js
@@ -1,92 +1,106 @@
- 
- const apiUrl = `https://wincacademydatabase.firebaseio.com/marcel/tasks.json`;
 
- const getToDoList = async function () { 
-       
+const apiUrl = `https://wincacademydatabase.firebaseio.com/marcel/tasks.json`;
+
+const getToDoList = async function () {
+
     try {
-    const res =  await fetch(apiUrl, { method: 'GET' });
+        const res = await fetch(apiUrl, { method: 'GET' });
         const data = await res.json();
         let tasks = Object.keys(data).map(key => ({
             id: key,
             description: data[key].description,
             done: data[key].done
         }));
-
-        tasks.forEach (task => {
+       
+        tasks.forEach(task => {
             const removeTask = async function () {
-                const apiRemUrl = `https://wincacademydatabase.firebaseio.com/marcel/tasks/${task.id}.json`;
-                 try { 
-                  const res = await fetch(apiRemUrl, { method: 'DELETE',                     
-                       })
-                      location.reload(true);
-                       }
-                   catch (error) {
-                   console.log(error)}  
-       
-               };
+                try {
+                    const apiRemUrl = `https://wincacademydatabase.firebaseio.com/marcel/tasks/${task.id}.json`;
+                    await fetch(apiRemUrl, {
+                        method: 'DELETE',
 
+                    })
+
+                    location.reload(true);
+                }
+                catch (error) {
+                    console.log(error)
+                } 
+            }
+                const changeBoolean = async function () {
+                    try {
+                       const apiRemUrl = `https://wincacademydatabase.firebaseio.com/marcel/tasks/${task.id}.json`;
+                        await fetch(apiRemUrl, {
+                            method: 'PUT',
+                            body: JSON.stringify({
+                                description: task.description,
+                                done: !task.done
+                            })
+                        })
+                        location.reload(true);
+                    }
+                    catch (error) {
+                        console.log(error)
+                    }
+            };
+          
+                document.getElementById("items");
+                const checkBox = document.createElement('input');
+                const newP = document.createElement('p');
+                checkBox.type = "checkbox";
+                checkBox.id = "checkbox";
+                checkBox.addEventListener("click", changeBoolean)
+
+                let label = document.createElement('label');
               
-            document.getElementById("items");
-            const checkBox = document.createElement('input');
-            const newP = document.createElement('p');
-            checkBox.type = "checkbox"; 
-            checkBox.id = "checkbox"; 
-            let label = document.createElement('label'); 
-            label.htmlFor = "id";
-            label.appendChild(document.createTextNode(`${task.description}`)); 
-            items.appendChild(checkBox); 
-            items.appendChild(label); 
-            
-            const trashBin = document.createElement("img");
-            trashBin.src = 'trash-delete-icon.png';
-            trashBin.addEventListener ("click", removeTask)
-            items.appendChild(trashBin);
-            items.appendChild(newP); 
-           
-            checkBox.addEventListener("click",function() {
-            if (checkBox.checked) { 
-            label.style.textDecoration="line-through";
-            label.style.fontStyle="italic"}
-            else {
-            label.style.textDecoration= "none";
-            label.style.fontStyle="normal"
-            }
-            });
-          })
-            
-            }
-        catch (error) {
-            console.log(error)}   
-                   
-        }
-       
+                if (task.done) {
+                    label.style.textDecoration = "line-through";
+                    label.style.fontStyle = "italic";
+                }
+
+                label.appendChild(document.createTextNode(`${task.description}`));
+                items.appendChild(checkBox);
+                items.appendChild(label);
+
+                const trashBin = document.createElement("img");
+                trashBin.src = 'trash-delete-icon.png';
+                trashBin.addEventListener("click", removeTask)
+                items.appendChild(trashBin);
+                items.appendChild(newP);
+                 
+}); 
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+}
+
 
 getToDoList();
 
-               
-        const addTask = async function () {
-            const task = document.getElementById("inputfield").value; 
-        
-            try { 
-           const res =  await fetch(apiUrl, { method: 'POST', 
-             body: JSON.stringify ({
-                 description: task,
-                 done: false
-                }) 
-            })
-           location.reload(true);
-        }
-        catch (error) {
-            console.log(error)}   
-                                
-                }
-        
-        let newTask= document.getElementById('button');
-        button.addEventListener("click", addTask);
-       
-             
-                
-              
- 
 
-    
+const addTask = async function () {
+    const task = document.getElementById("inputfield").value;
+
+    try {
+        await fetch(apiUrl, {
+            method: 'POST',
+            body: JSON.stringify({
+                description: task,
+                done: false
+            })
+        })
+        location.reload(true);
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+}
+
+let newTask = document.getElementById('button');
+button.addEventListener("click", addTask);
+
+
